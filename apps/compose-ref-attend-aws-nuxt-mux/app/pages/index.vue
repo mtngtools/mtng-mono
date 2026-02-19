b2332c469b79cce5f9525cc9c5f59d9cb38d9afb
Jason Bulson <3855668+jbulson@users.noreply.github.com>
2026-02-19 08:05:03 -0500

    chore: remove example

--- apps/compose-ref-attend-aws-nuxt-mux/app/pages/index.vue
+++ apps/compose-ref-attend-aws-nuxt-mux/app/pages/index.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
     <h1>compose-ref-attend-aws-nuxt-mux</h1>
-    <p>Reference attendee site. Visit <NuxtLink to="/example">/example</NuxtLink> to see frame-vue in action.</p>
   </div>
 </template>
